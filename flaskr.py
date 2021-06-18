25d538ccaa5ee42d50276e295d992225d5a354e6
Fábio Medeiros <fabmed@gmail.com>
2021-06-18 16:49:50 +0000

    teste

---
+++ flaskr.py
@@ -0,0 +1,16 @@
+import sqlite3
+from flask import Flask
+
+# config
+DATABASE = './flaskr.db'
+SECRET_KEY = 'pudim'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+
+# aplicacao
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+def connect_db():
+    return sqlite3.connect(DATABASE)
+
